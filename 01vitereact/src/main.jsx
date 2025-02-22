20499006f405884f12c152649e6b61c185eef4f6
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-02-22 15:43:35 +0000

    new

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+function Testing (){
+  return(
+    <>
+    <h1>tested</h1>
+    </>
+  )
+}
+
+// const customReact = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click here to go to google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>vimsit</a>
+)
+
+const anotherUser = 'chai aur code'
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target:'_blank'},
+  'click me to visit google',
+   anotherUser
+)
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
