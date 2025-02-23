d7d23ead12dd9ca96dc506a4a73dcbf766660263
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-02-23 23:04:03 +0530

    Update hooks.jsx

--- 01vitereact/src/hooks.jsx
+++ 01vitereact/src/hooks.jsx
@@ -1,1 +1,29 @@
+import React, { useState } from "react";
 
+
+function States(){
+    const [button,Setbutton] = useState(10)
+
+    function addition(){
+        Setbutton(button + 1)
+    }
+    function minus(){
+        Setbutton(button - 1)
+    }
+    
+    return(
+    <>  
+    <button onClick={addition}>addition</button>
+    <br/>
+    <button onClick={minus}>minus</button>
+    <h1>this is number 1 {button}</h1>
+    <h1>this is number 2 {button}</h1>
+    <h1>this is number 3 {button}</h1>
+    <h1>this is number 4 {button}</h1>
+    </>
+
+    )
+
+}
+
+export default States
