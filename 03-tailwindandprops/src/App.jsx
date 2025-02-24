a4e44f00c23515b10c4d6d68fd19758118e7d1fc
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-02-24 17:34:38 +0000

    tailwindandprops

---
+++ 03-tailwindandprops/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+    let myObj = {
+      username: "dew corpo",
+      age: 24
+    }
+  
+    let newarray = [1,2,3,4]
+  
+    return (
+      <>
+      <h1 className="text-3xl font-bold bg-green-400 p-4 rounded-xl mb-4">
+      Tailwind test
+      </h1>
+      <Card username="Fenil" btnText="click Me"/>
+      <Card username='Dew Corp'/>
+      </>
+  )
+}
+
+export default App
