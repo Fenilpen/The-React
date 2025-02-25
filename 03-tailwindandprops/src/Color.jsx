7e8f84f934de6776926eecee35b774e42e047149
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-02-26 01:28:00 +0530

    Update Color.jsx

--- 03-tailwindandprops/src/Color.jsx
+++ 03-tailwindandprops/src/Color.jsx
@@ -1,30 +1,19 @@
 
 
 function Randomcolor (){
-    function ChangeBlack(){
-        document.body.style.backgroundColor = 'black'
-    }
-    function ChangeRed(){
-        document.body.style.backgroundColor = 'red'
-    }
-    function ChangeGreen(){
-        document.body.style.backgroundColor = 'green'
-    }
-    function ChangeBlue(){
-        document.body.style.backgroundColor = 'blue'
-    }
-    function ChangeOrange(){
-        document.body.style.backgroundColor = 'orange'
+    function changecolor (color){
+        console.log(color);
+        document.body.style.backgroundColor = color
     }
     return(
         <>
         <h1 className="mb-4 font-semibold ">Random Color</h1>
         <div className="flex flex-row-reverse space-x-2 space-x-reverse border- rounded-xl p-2 bg-white" >
-        <button className="bg-orange-400" onClick={ChangeOrange}>Orange</button>
-        <button className="bg-blue-400" onClick={ChangeBlue}>Blue</button>
-        <button className="bg-green-400" onClick={ChangeGreen}>Green</button>
-        <button className="bg-red-400" onClick={ChangeRed}>Red</button>
-        <button className="bg-gray-400" onClick={ChangeBlack}>Black</button>
+        <button className="bg-orange-400" onClick={()=>{changecolor('orange')}}>Orange</button>
+        <button className="bg-blue-400" onClick={()=>{changecolor('blue')}}>Blue</button>
+        <button className="bg-green-400" onClick={()=>{changecolor('green')}}>Green</button>
+        <button className="bg-red-400" onClick={()=>{changecolor('red')}}>Red</button>
+        <button className="bg-gray-400" onClick={()=>{changecolor('black')}}>Black</button>
         </div>
         </>
     )
